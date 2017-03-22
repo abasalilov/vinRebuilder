f6ac2d17f093167dfcd0765e1d0a285e46a61e1b
ALEKSANDR BASALILOV <aleks@aleksandrs-mbp.attlocal.net>
2017-03-22 00:55:11 -0700

    test

--- test.js
+++ test.js
@@ -1,4 +1,49 @@
+window.import chai from './node_modules/chia.js';
+import chai from './node_modules/chia.js';
+import mocha from './node_modules./mocha.js';
 
+  mocha.setup('bdd');
+  var expect = chai.expect;
+
+  describe("test", function() {
+
+    beforeEach(function(){
+
+  });
+
+
+   describe('test specific behavior', function(){
+      it('does something', function() {
+        a.b('c');
+        expect(a.b()).to.equal('c');
+      });
+
+      it('does something else', function() {
+        a.b('c');
+        expect(a.b()).to.equal('c');
+      });
+    });
+
+  window.mochaPhantomJS ? mochaPhantomJS.run() : mocha.run();
+});
+}
+
+test(chai)l;
+
+
+
+//SAMPLES TO TEST
+
+//19UUA56602A801534
+//VIN Description: 2002 Acura TL
+//2HNYD28809H002590
+//VIN Description: 2009 Acura MDX
+// 5YJSA1DG9DFP14705
+//VIN Description: 2013 Tesla Model S
+//1FDCA14X0L0013299
+//VIN Description: 1990 Ford Aerostar
+//JF1ZNAA18D2706977
+//Make: TOYOTA || Model: SCION FR-S || Year: 2013
 //SAMPLES TO TEST
 
 //19UUA56602A801534
